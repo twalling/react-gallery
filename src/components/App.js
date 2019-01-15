8839e348a28db71ef4ebdef5f7e4e86a24ff2813
Tim Walling <mail@timwalling.com>
2019-01-14 21:44:55 -0500

    modified URL in app for photo api

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,7 @@ class App extends Component {
 
     // Get the photos for beaches, mountains, and lakes
     getPhotos = (tag, photos) => {
-        axios.get(`http://localhost:9000/photos?method=flickr.photos.search&tags=${tag}&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
+        axios.get(`/photos?method=flickr.photos.search&tags=${tag}&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
         .then(response => {
             this.setState({
                 [photos]: response.data.photos.photo
@@ -57,7 +57,7 @@ class App extends Component {
         });
         // If there is a query, fetch the data from Flickr
         if (query) {
-            axios.get(`http://localhost:9000/photos?method=flickr.photos.search&tags=${query}&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
+            axios.get(`/photos?method=flickr.photos.search&tags=${query}&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
             .then(response => {
                 this.setState({
                     showResults: response.data.photos.photo.length > 0,
